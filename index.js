eb7bf830c3c3f563dd2f798c3ed954d62dad53de
BookmDan <danoh329@gmail.com>
2023-07-23 16:33:08 -0700

    wow that was confusing. still have to review this

--- index.js
+++ index.js
@@ -11,6 +11,45 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1));
 }
+
+// function toTitleCase(sentence) {
+//   const words = sentence.split(' ');
+//   const capitalizeedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//   return capitalizedWords.join(' ');
+// }
+
+// function capitalizeEachWord(sentence) {
+//   const words = sentence.split(' ');
+//   const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//   return capitalizedWords.join(' ');
+// }
+
+// Function to convert all tutorial titles to title case using forEach
+
+
+// function titleCased() {
+//   const titleCasedTutorials = [];
+//   tutorials.forEach((tutorial) => {
+//     titleCasedTutorials.push(toTitleCase(tutorial));
+//   });
+//   return titleCasedTutorials;
+// }
+
+
+function titleCased() {
+  return tutorials.map((tutorial) => toTitleCase(tutorial));
+}
+
+
+// function capitalizeEachWord(sentence) {
+//   const words = sentence.split(' ');
+//   const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//   return capitalizedWords.join(' ');
+// }
+
+// const sentence = "What Is The Difference Between StopPropagation And PreventDefault?";
+// const capitalizedSentence = capitalizeEachWord(sentence);
+// console.log(capitalizedSentence); // Output: "What Is The Difference Between StopPropagation And PreventDefault?"
